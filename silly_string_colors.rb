7dc3b81684adc00f96222e62af6c9cb204e026cb
d-m-u <16326669+d-m-u@users.noreply.github.com>
2019-10-11 15:10:42 -0400

    Update silly_string_colors.rb

--- silly_string_colors.rb
+++ silly_string_colors.rb
@@ -1,9 +1,29 @@
- class String
-  def red;            puts "\e[31m#{self}\e[0m" end
-  def green;          puts "\e[32m#{self}\e[0m" end
-  def brown;          puts "\e[33m#{self}\e[0m" end
-  def blue;           puts "\e[34m#{self}\e[0m" end
-  def magenta;        puts "\e[35m#{self}\e[0m" end
-  def cyan;           puts "\e[36m#{self}\e[0m" end
-  def bold;           puts "\e[1m#{self}\e[22m" end
+class String
+  def black;          "\e[30m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
+
+  def bg_black;       "\e[40m#{self}\e[0m" end
+  def bg_red;         "\e[41m#{self}\e[0m" end
+  def bg_green;       "\e[42m#{self}\e[0m" end
+  def bg_brown;       "\e[43m#{self}\e[0m" end
+  def bg_blue;        "\e[44m#{self}\e[0m" end
+  def bg_magenta;     "\e[45m#{self}\e[0m" end
+  def bg_cyan;        "\e[46m#{self}\e[0m" end
+  def bg_gray;        "\e[47m#{self}\e[0m" end
+
+  def bold;           "\e[1m#{self}\e[22m" end
+  def italic;         "\e[3m#{self}\e[23m" end
+  def underline;      "\e[4m#{self}\e[24m" end
+  def blink;          "\e[5m#{self}\e[25m" end
+  def reverse_color;  "\e[7m#{self}\e[27m" end
+
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
 end
